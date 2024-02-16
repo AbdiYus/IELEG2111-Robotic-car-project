72657a16ffa00406bd731edb8e1f52eb9c5cb7b5
AbdiYus <yusabdirahman@gmail.com>
2024-02-16 14:57:40 +0100

    testing

--- Car/src/main.cpp
+++ Car/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 #include <Encoder.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int myFunction(int x, int y);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
+  int result = myFunction(2, 3);
+  Serial.println(result);
   // put your main code here, to run repeatedly:
 }
 
