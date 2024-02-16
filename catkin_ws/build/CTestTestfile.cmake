ecffc3498b226299758e1ffe6ff98853b2c8fdf6
ros <ros@ros1.myguest.virtualbox.org>
2024-02-16 15:54:38 +0100

    added workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/src
+# Build directory: /home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
