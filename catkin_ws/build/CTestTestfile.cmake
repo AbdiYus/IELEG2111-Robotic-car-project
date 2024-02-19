28c3eb7831cdc1c79b2a7c8f31c881d248c7d3b0
didiernm <didier.mupenda@ntnu.no>
2024-02-19 15:10:57 +0100

    hector slam, re-make catikin_make, lagt til maps

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -1,7 +1,21 @@
 # CMake generated Testfile for 
-# Source directory: /home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/src
-# Build directory: /home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/build
+# Source directory: /home/didmup/IELEG2111-Robotic-car-project/catkin_ws/src
+# Build directory: /home/didmup/IELEG2111-Robotic-car-project/catkin_ws/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("hector_slam/hector_geotiff_launch")
+subdirs("hector_slam/hector_slam")
+subdirs("hector_slam/hector_slam_launch")
+subdirs("hector_slam/hector_map_tools")
+subdirs("hector_slam/hector_nav_msgs")
+subdirs("hector_slam/hector_geotiff")
+subdirs("hector_slam/hector_geotiff_plugins")
+subdirs("hector_slam/hector_marker_drawing")
+subdirs("hector_slam/hector_compressed_map_transport")
+subdirs("hector_slam/hector_imu_attitude_to_tf")
+subdirs("hector_slam/hector_imu_tools")
+subdirs("hector_slam/hector_map_server")
+subdirs("hector_slam/hector_trajectory_server")
+subdirs("hector_slam/hector_mapping")
--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export PWD='/home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/ros/Desktop/IELEG2111-Robotic-car-project/catkin_ws/src:$ROS_PACKAGE_PATH"
+export PWD='/home/didmup/IELEG2111-Robotic-car-project/catkin_ws/build'
