cc2ca8602a730e37fbe04d008cad5acbed12b961
AbdiYus <yusabdirahman@gmail.com>
2024-02-19 15:31:20 +0100

    remove build folder

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mosa/IELEG2111-Robotic-car-project/catkin_ws/src
-# Build directory: /home/mosa/IELEG2111-Robotic-car-project/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("mpu_6050_driver")
