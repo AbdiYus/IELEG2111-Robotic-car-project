a2e3e1cc96fdb284776f57f06d7f5e4bd309b17a
Mosazghi <fest121@hotmail.com>
2024-05-15 16:34:54 +0200

    delete build

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mosa/IELEG2111-Robotic-car-project/catkin_ws/src'
-whitelisted_packages = 'mpu_6050_driver'.split(';') if 'mpu_6050_driver' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mosa/IELEG2111-Robotic-car-project/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/mosa/IELEG2111-Robotic-car-project/catkin_ws/devel;/opt/ros/noetic' != '' else []
