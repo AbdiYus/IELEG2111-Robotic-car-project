7a53793f84853682eba4cc205a1cb90cf067af64
AbdiYus <yusabdirahman@gmail.com>
2024-06-07 11:56:23 +0200

    rm build dir

--- catkin_ws/build/ros_imu_bno055/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mosa/IELEG2111-Robotic-car-project/catkin_ws/src/ros_imu_bno055
-# Build directory: /home/mosa/IELEG2111-Robotic-car-project/catkin_ws/build/ros_imu_bno055
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
